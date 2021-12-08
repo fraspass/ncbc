fd05980c9123615ff44c334a617bc210d8597b9d
fraspass <fraspass@outlook.com>
2021-12-08 10:30:51 +0000

    requirements

--- lib/setup.py
+++ lib/setup.py
@@ -1,14 +1,16 @@
 from setuptools import setup
 
 setup(
-	name="lda_clust",
-	version="1.0",
+	name='lda_clust',
+	version='1.0',
 	packages=[
-		"lda_clust",
+		'lda_clust',
 	],
 	install_requires=[
-		"numpy",
-		"scipy",
+		'numpy',
+		'scipy',
 		"collections",
+		'gensim',
+		'sklearn'
 	],
 )
